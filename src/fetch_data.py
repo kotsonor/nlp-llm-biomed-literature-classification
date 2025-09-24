88f0a016d7f4f42983369665513c5614d8a4724d
sonor <316195@uwr.edu.pl>
2025-09-24 12:49:22 +0200

    doc: add example command for fetch_data.py

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -41,3 +41,11 @@ if __name__ == "__main__":
     p.add_argument("--email")  # dla pubmed
     args = p.parse_args()
     main(args)
+
+
+# python src/fetch_data.py \
+#   --mode pubmed \
+#   --dataset ./data/raw/amyloid-raw-22-08-2025.csv \
+#   --column PMID \
+#   --output ./data/fetched/pubmed-amyloid-22-08-2025.csv \
+#   --email test@example.com
